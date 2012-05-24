06ee39632e005ef5ebc38984113fe47f15d29e93
Richard O. Legendi <richard.legendi@gmail.com>
2012-05-24 15:36:15 +0200

    Added Specs2 Finder and Runner placeholders

---
+++ org.scala-ide.sdt.specs2/src/org/scalatest/specs2/Spec2Runner.scala
@@ -0,0 +1,21 @@
+package org.scalatest.specs2
+
+import org.scalatest.Suite
+import org.scalatest.Filter
+import org.scalatest.Stopper
+import org.scalatest.Reporter
+import org.scalatest.Distributor
+import org.scalatest.Tracker
+import org.scalatest.Resources
+import org.scalatest.events.InfoProvided
+import org.scalatest.events.NameInfo
+import org.scalatest.Style
+import scala.collection.mutable.Stack
+
+@Style("org.scalatest.specs.Spec2Finder")
+class Spec2Runner {
+//class Spec2Runner(specificationClass: Class[_ <: Specification]) extends Suite { 
+//class Spec2Runner(specificationClass: Class[_ <: ]) extends Suite {
+  
+  // TODO Specs2 arguments?
+}
